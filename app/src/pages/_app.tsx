ffa88fce558dbe735e55b07e30ccfe41c569e6c4
Harsha Somisetty <harshasomisetty7@gmail.com>
2022-06-27 18:21:09 -0400

    inited nextjs and tailwind app

---
+++ app/src/pages/_app.tsx
@@ -0,0 +1,56 @@
+import "../styles/global.css";
+import React from "react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import {
+  LedgerWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+  SolletExtensionWalletAdapter,
+  SolletWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import { AppProps } from "next/app";
+import { FC, useMemo } from "react";
+import "../styles/global.css";
+// Use require instead of import since order matters
+require("@solana/wallet-adapter-react-ui/styles.css");
+
+// import Navbar from "../components/Navbar";
+
+// const logo = require("./logo.png");
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  // Can be set to 'devnet', 'testnet', or 'mainnet-beta'
+  const network = WalletAdapterNetwork.Devnet;
+  // const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  const endpoint = useMemo(() => "http://localhost:8899");
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SlopeWalletAdapter(),
+      new TorusWalletAdapter(),
+      new LedgerWalletAdapter(),
+    ],
+    [network]
+  );
+
+  return (
+    <div className="m-2 text-white font-mono h-screen">
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect>
+          <WalletModalProvider>
+            <Component {...pageProps} />
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </div>
+  );
+};
+
+export default MyApp;
